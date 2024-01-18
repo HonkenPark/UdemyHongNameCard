41d7dadabe6d3e19c01b6e464635c6946c8b14ba
Honken Park <honken.park@gmail.com>
2024-01-18 20:10:54 +0900

    TYPE(FEAT): Debug Syntax Error

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidjet {
-	const ({super.key});
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-		return const Scaffold(
-			body: Center(
-				child: Image.asset(
-					'assets/business_card.png',
-					width: 180,
-					height: 180,
-				),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          '/assets/business_card.png',
+          width: 180,
+          height: 180,
+        ),
+      ),
+    );
+  }
 }
