b76b12569c254d447814d4f0903754251ae262d8
Honken <76989036+HonkenPark@users.noreply.github.com>
2024-01-18 11:23:52 +0900

    splash_screen.dart 업데이트

---
+++ lib/splash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dartk;
+
+class SplashScreen extends StatelessWidjet {
+	const ({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return const PlaceHolder();
+	}
+}
